d30371ef214c3c0698b2b4c05a47f8f810716f02
Cristian <crisosbor@gmail.com>
2016-05-12 12:38:28 +0200

    Campos

---
+++ Assets/FingerSoccerKit/C#-Assets/Scripts-C#/ConsoleScreen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ConsoleScreen : MonoBehaviour {
+
+	public Text escribeLineas;
+	public static string myLog;
+	private string output ="";
+	private string stack ="";
+	// Update is called once per frame
+	public static bool mostrarConsola=false;
+	public GameObject consola;
+
+	void HandleLogCallback (string condition, string stackTrace, LogType type)
+	{
+		output = condition;
+		stack = stackTrace;
+		myLog += "\n" + output;
+	}
+
+	void Update () {
+		UnityEngine.Application.RegisterLogCallback (HandleLogCallback);
+		escribeLineas.text = myLog;
+
+
+	}
+
+	public void showConsole(){
+		mostrarConsola = true;
+		if (mostrarConsola == true) {
+			consola.SetActive (true);
+		} else {
+			consola.SetActive (false);
+		}
+		mostrarConsola = false;
+	}
+
+
+}
